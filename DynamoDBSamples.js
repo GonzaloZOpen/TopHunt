0474b675fc6c05f742d72417068681a69d3597bc
Gonzalo <zapozapico@gmail.com>
2024-01-04 17:32:10 -0300

    04/01/2024 Version Multiples AdSlots por pagina Desktop y Mobile

---
+++ DynamoDBSamples.js
@@ -0,0 +1,37 @@
+//Create
+
+{
+    "operation": "create",
+    "payload": {
+      "Item": {
+        "Id": "1234ABCD",
+        "requests": 0,
+        "views": 0
+      }
+    }
+  }
+
+
+//Read
+
+
+//Update
+
+{
+    "operation": "update",
+    "payload": {
+        "Key": {
+            "Id": "1234ABCD"
+        },
+        "AttributeUpdates": {
+            "number": {
+                "Value": 10
+            }
+        }
+    }
+}
+
+
+//Delete
+
+//Echo
