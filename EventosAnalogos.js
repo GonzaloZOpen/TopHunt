19c21260e78d9e8079cc00ff8a0464d1677fa8e1
Gonzalo <zapozapico@gmail.com>
2024-01-04 17:36:25 -0300

    04/01/2024 Version Multiples AdSlots por pagina Desktop y Mobile

---
+++ EventosAnalogos.js
@@ -0,0 +1,35 @@
+//SlotRequestedA
+
+try {
+    //display()
+    window.dataLayer.push({event: "slotRequestedA", adUnit: adUnit, bool: true, message: "Requested correctly"})
+} catch (e) {
+    window.dataLayer.push({event: "slotRequestedA", adUnit: adUnit, bool: false, message: e.message})
+}
+
+
+//SlotReceivedA
+
+
+//SlotRenderEndedA
+
+
+//SlotOnLoadA
+
+
+//SlotVisibilityChangeA
+
+
+//SlotViewabilityA
+
+
+
+//MutationObserver
+
+
+
+
+
+
+
+observer.disconnect();
